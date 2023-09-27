66e3ae5801926eb2774466a7a488d298911aff06
Saravanakumar Ganesh <kgsaravana87o@gmail.com>
2023-09-27 12:04:55 -0400

    implement addAlien function

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -3,12 +3,20 @@ package com.example.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.demo.model.Alien;
+
 @Controller
 public class HomeController {
 	
 	@RequestMapping("/")
 	public String home() {
 		System.out.println("in home");
+		return "home.jsp";
+	}
+	
+	@RequestMapping("/addAlien")
+	public String addAlien(Alien alien) {
+		System.out.println("aid " + alien.getAid());
 		return "home.jsp";
 	}
 
--- src/main/webapp/home.jsp
+++ src/main/webapp/home.jsp
@@ -7,6 +7,10 @@
 <title>Insert title here</title>
 </head>
 <body>
-	Welcome Java!
+	<form action="addAlien">
+		<input type="text" name="aid"><br>
+		<input type="text" name="aname"><br>
+		<input type="submit"><br>
+	</form>
 </body>
 </html>
