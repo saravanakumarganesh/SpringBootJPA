b5f2a05d367e7bfd9941fc007d454e785059a4e7
Saravanakumar Ganesh <kgsaravana87o@gmail.com>
2023-09-28 11:58:05 -0400

    implement rest controllers

--- src/main/java/com/example/demo/controller/AlienController.java
+++ src/main/java/com/example/demo/controller/AlienController.java
@@ -2,8 +2,10 @@ package com.example.demo.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.example.demo.dao.AlienRepo;
@@ -42,6 +44,20 @@ public class AlienController {
 		mv.setViewName("showAlien.jsp");
 	
 		return mv;
+	}
+	
+	@RequestMapping("/aliens")
+	@ResponseBody
+	public String getAliens() {
+		
+		return repo.findAll().toString();
+	}
+	
+	@RequestMapping("/alien/{aid}")
+	@ResponseBody
+	public String getAlienRest(@PathVariable int aid) {
+		return repo.findById(aid).toString();
+		
 	}
 
 }
