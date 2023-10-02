fea7885147f1a7efc526022a9a30855f3a4448e8
Saravanakumar Ganesh <kgsaravana87o@gmail.com>
2023-10-02 14:27:25 -0400

    implement delete alien

--- src/main/java/com/example/demo/controller/AlienController.java
+++ src/main/java/com/example/demo/controller/AlienController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -78,6 +79,13 @@ public class AlienController {
 	public Alien addAlienRest(@RequestBody Alien alien) {
 		restRepo.save(alien);
 		return alien;
+	}
+	
+	@DeleteMapping("/alien/{aid}")
+	@ResponseBody
+	public String deleteAlienRest(@PathVariable int aid) {
+		restRepo.deleteById(aid);
+		return "deleted";
 	}
 
 }
