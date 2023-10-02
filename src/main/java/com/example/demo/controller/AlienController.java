1dc7bad74155bf58ef6fbbc217bc6eb576001626
Saravanakumar Ganesh <kgsaravana87o@gmail.com>
2023-10-02 12:28:43 -0400

    accept data from raw request body'

--- src/main/java/com/example/demo/controller/AlienController.java
+++ src/main/java/com/example/demo/controller/AlienController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -74,7 +75,7 @@ public class AlienController {
 //	@RequestMapping("/alien")
 	@PostMapping("/alien")
 	@ResponseBody
-	public Alien addAlienRest(Alien alien) {
+	public Alien addAlienRest(@RequestBody Alien alien) {
 		restRepo.save(alien);
 		return alien;
 	}
